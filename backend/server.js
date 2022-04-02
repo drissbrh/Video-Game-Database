1c5964cb72f55203562b9658875f501aa1c18fc4
drissbrh <lanigabo8@gmail.com>
2022-04-02 02:05:10 +0000

    Final Deployment prep

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,25 @@ const express = require("express");
 const colors = require("colors");
 const connectDB = require("./config/db");
 const gameRouter = require("./routes/gameRoutes");
+const path = require("path");
 
 const app = express();
-
+app.use(express.json());
 connectDB();
-app.get("/", (req, res) => {
-  res.send("Bad bad not good");
-});
 
 app.use("/api/v1/games", gameRouter);
-const PORT = process.env.PORT || 5001;
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api Is Running");
+  });
+}
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on PORT ${PORT}`.yellow.bold);
 });
