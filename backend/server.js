6a8d7398baecdb1e7212ab18c4a5f5cbf4925a95
drissbrh <lanigabo8@gmail.com>
2022-04-10 01:41:39 +0000

    gh

--- backend/server.js
+++ backend/server.js
@@ -18,11 +18,10 @@ app.use(express.json());
 app.use("/api/v1/games", gameRouter);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
-  );
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running....");
