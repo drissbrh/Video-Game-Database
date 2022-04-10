1c9af82465973b6c0dd642c60cb3348bdbbd279f
drissbrh <lanigabo8@gmail.com>
2022-04-10 01:25:24 +0000

    nacklo lcashere

--- backend/server.js
+++ backend/server.js
@@ -10,21 +10,22 @@ dotenv.config();
 connectDB();
 const app = express();
 
-app.use(express.json());
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
+app.use(express.json());
 app.use("/api/v1/games", gameRouter);
 
-const __dirname = path.resolve();
-
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"));
-  });
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
 } else {
   app.get("/", (req, res) => {
-    res.send("Api Is Running");
+    res.send("API is running....");
   });
 }
 const PORT = process.env.PORT || 5000;
